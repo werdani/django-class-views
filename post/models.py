cbad67e3f46259ca8badd8265c4ba7b719b26fc0
werdani <https://github.com/werdani/django-job-board.git>
2020-09-16 03:40:10 +0200

    add new {post}

---
+++ post/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=1000)
+    created_at=models.TimeField(default=timezone.now)
+    image=models.ImageField(upload_to='post-img/')
+
+
+
+
+    class Meta:
+        verbose_name=("Post")
+        verbose_name_plural=("Posts")
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reversed("post_detail",kwargs={"pk":self.pk})
